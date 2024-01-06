1f87c9cdcd0001ecad8e2e967ee9ba425e677819
arsenaaal <155857613+arsenaaal@users.noreply.github.com>
2024-01-07 01:59:33 +0200

    Add files via upload

---
+++ src/main/java/com/example/telegrambot/TelegrambotApplication.java
@@ -0,0 +1,14 @@
+package com.example.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
